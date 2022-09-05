361e233ddc5f42316040a4b3a2531b72095754a0
GEMMEE <jr.gamachu@gmail.com>
2022-09-06 03:30:13 +0530

    the 3 languages

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
 
-// script that prints C is fun, Python is cool and JavaScript is
-// amazing on 3 lines
+// script that prints C is fun, Python is cool and JavaScript is amazing on 3 lines;
 
 const myVar = 'C is fun\nPython is cool\nJavascript is amazing';
 console.log(myVar);
