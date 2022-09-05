7722e0ba07b0c1bf77cbf729cac08d14d99f5a70
GEMMEE <jr.gamachu@gmail.com>
2022-09-06 03:06:01 +0530

    COOL

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+
+// script that prints C is fun, Python is cool and JavaScript is
+// amazing on 3 lines
+
+const myVar = 'C is fun\nPython is cool\nJavascript is amazing';
+console.log(myVar);
