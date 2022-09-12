63128afa1b883ed327fbf7fe3e5c8acd75730f58
GEMMEE <jr.gamachu@gmail.com>
2022-09-12 22:00:19 +0530

    count 89

---
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -0,0 +1,5 @@
+-- displays the number of records with id = 89 in the table first_table
+-- of the database hbtn_0c_0 in my local MySQL server
+-- The database name will be passed as an argument of mysql command
+
+SELECT COUNT(*) FROM first_name WHERE id = 89; 
