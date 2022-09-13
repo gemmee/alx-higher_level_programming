a633ce4f0bdf8db97c27691eff0f91a67c480355
GEMMEE <jr.gamachu@gmail.com>
2022-09-13 15:44:49 +0530

    body size

---
+++ 0x10-python_network_0/1-body.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# A bash script that takes in a URL, sends a GET request to that URL, 
+# and displays the size(in bytes) of the body of the response.
+
+curl -sI "$1" | grep "Content-Length:" | cut -d' ' -f2
