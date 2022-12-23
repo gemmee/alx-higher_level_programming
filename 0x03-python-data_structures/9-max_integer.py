6631d0a89b81d5207c19da6a43616a7b7e018416
Jaba <jr.gamachu@gmail.com>
2022-12-23 19:25:46 +0530

    Return maximum integer

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,11 @@
+#!/bin/usr/python3
+
+def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return None
+    max = my_list[0]
+    for item in range(len(my_list)):
+        if my_list[item] > max:
+            max = my_list[item]
+    return max
+
