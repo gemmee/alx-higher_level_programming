7ac586f8359a1215e435c8ff2fa014a774f436ce
Jaba <jr.gamachu@gmail.com>
2022-12-23 19:32:26 +0530

    Find the max

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,6 +1,11 @@
 #!/bin/usr/python3
 
 def max_integer(my_list=[]):
+    """
+    Finds and returns the biggest integer of a list.
+
+    my_list: the list
+    """
     if len(my_list) == 0:
         return None
     max = my_list[0]
@@ -8,4 +13,3 @@ def max_integer(my_list=[]):
         if my_list[item] > max:
             max = my_list[item]
     return max
-
