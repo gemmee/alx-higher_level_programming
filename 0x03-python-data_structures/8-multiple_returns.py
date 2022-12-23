58b146c66382fa3d4a9fed49883f4b2c5cd66b2f
Jaba <jr.gamachu@gmail.com>
2022-12-23 19:01:07 +0530

    Return tuple

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,4 +1,5 @@
 #!/bin/usr/python3
+
 def multiple_returns(sentence):
     """
     Returns a tuple with the length of a string and its first character.
@@ -10,4 +11,4 @@ def multiple_returns(sentence):
         f_char = None
     else:
         f_char = sentence[0]
-    return (s_len, f_char) 
+    return ((s_len, f_char))
