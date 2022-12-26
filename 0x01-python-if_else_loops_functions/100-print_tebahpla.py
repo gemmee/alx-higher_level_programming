cf3ad83ce645bd3db3f96ff09a6a826eaa298398
Jaba <jr.gamachu@gmail.com>
2022-12-26 18:57:57 +0530

    Print alternated reverse alpha

---
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -0,0 +1,10 @@
+#!/bin/usr/python3
+
+def printReverse():
+    '''
+    prints the ASCII alphabet, in reverse order, alternating lowercase
+    and uppercase.
+    '''
+    for i in range(90, 64, -1):
+        print("{}".format(chr(i) if i % 2 else chr(i + 32)), end='')
+printReverse()
