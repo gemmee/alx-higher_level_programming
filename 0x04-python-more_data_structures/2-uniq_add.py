7fdc71327c796f0759a31feadbcbdf5395c03226
Jaba <jr.gamachu@gmail.com>
2022-12-28 06:20:56 +0530

    Add unique integers in a list

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,11 @@
+def uniq_add(my_list=[]):
+    """
+    Adds all unique integers in a list.
+
+    my_list: the list
+    """
+    sum = 0
+    for i in range(len(my_list)):
+        if my_list[i] not in my_list[i + 1:]:
+            sum += my_list[i]
+    return sum
