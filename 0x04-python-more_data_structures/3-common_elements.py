fb40c11fe0a583b6cd7cc275184f04bd470993e0
Jaba <jr.gamachu@gmail.com>
2022-12-28 06:36:52 +0530

    Present in both

---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def common_elements(set_1, set_2):
+    """
+    Returns a set of common elements in two sets.
+    
+    set_1: the first set
+    set_2: the second set
+    """
+    return set_1.intersection(set_2)
