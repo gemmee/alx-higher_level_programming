44b674427bd5c3488e2d683dd8c4d3f36d3ee932
Jaba <jr.gamachu@gmail.com>
2022-12-29 23:50:44 +0530

    Prints an integer safely

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+def safe_print_integer(value):
+    '''
+    Prints an integer with '{:d}.format()' followed by a new line
+
+    value: the value to printed
+
+    Return: True if value has been correctly printed, otherwise False
+
+    Description:
+        You are not allowed to import any module
+        You are not allowed to use type()
+        Value can be any type (integer, string, etc.)
+    '''
+    try:
+        if value is not int:
+            raise TypeError("{} is not an integer".format(value))
+        print("{:d}".format(value))
+        return True
+    except (TypeError):
+        return False
