13e77ad4e54c4597f212112f08895d690f06702c
Jaba <jr.gamachu@gmail.com>
2022-12-29 13:10:12 +0530

    Best score

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+def best_score(a_dictionary):
+    """
+    Returns a key with a biggest integer value or
+    returns None if no integer found
+
+    a_dictionary: the dictionary with integers values
+    """
+    if a_dictionary is None:
+        return None
+    key_max = list(a_dictionary.keys())[0]
+    max = a_dictionary[key_max]
+    for k in a_dictionary:
+        if a_dictionary[k] > max:
+            max = a_dictionary[k]
+            key_max = k
+    return key_max
