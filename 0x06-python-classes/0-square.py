19780f1be2ed7da94ad0eb9ed49ab9a4f81eeacb
Jaba <jr.gamachu@gmail.com>
2023-01-05 02:54:19 +0530

    Create an empty class

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,3 +1,13 @@
 #!/usr/bin/python3
+"""Documentation for module 0-square
+
+This module demonstrates the creation of an empty string that defines
+a square without importing any module.
+
+"""
+
+
 class Square:
-    """An empty class"""
+    """An empty class. We could also type pass keyword on the line following
+    this docstring, but it won't be necessary as because of the presence of
+    this docstring"""
