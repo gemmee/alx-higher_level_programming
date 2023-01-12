41044d736cf4967d5d8342b9a1f29b256a224107
Jaba <jr.gamachu@gmail.com>
2023-01-13 03:50:12 +0530

    Loop to languages

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
 const message = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-// message.forEach((e) => { console.log(e); });
-for (const e of message) {
-  console.log(e);
+for (const m of message) {
+  console.log(m);
 }
