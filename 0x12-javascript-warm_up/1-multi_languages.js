1d06d29f2724cfc9ffc86285acd5d96e161c529a
Jaba <jr.gamachu@gmail.com>
2023-01-13 01:31:21 +0530

    Prints cool message about JS

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -2,5 +2,4 @@
 
 // script that prints C is fun, Python is cool and JavaScript is amazing on 3 lines;
 
-const myVar = 'C is fun\nPython is cool\nJavascript is amazing';
-console.log(myVar);
+console.log('C is fun\nPython is cool\nJavascript is amazing');
