4739c48f966949f4f78d6336a9163c0ff6b23a3c
Jaba <jr.gamachu@gmail.com>
2023-01-13 03:44:19 +0530

    Prints the message through loop

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+const message = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+// message.forEach((e) => { console.log(e); });
+for (const e of message) {
+  console.log(e);
+}
