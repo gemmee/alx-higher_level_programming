2a4ca564d55066999ed2eaa08ed4368b2d9c5a54
Jaba <jr.gamachu@gmail.com>
2023-01-13 04:29:15 +0530

    Object

---
+++ 0x12-javascript-warm_up/12-object.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+/*
+UPDATE THE CODE BELOW TO REPLACE 12 WITH 89
+*/
+const myObject = {
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+/*
+YOUR CODE HERE
+*/
+myObject.value = 89;
+console.log(myObject);
