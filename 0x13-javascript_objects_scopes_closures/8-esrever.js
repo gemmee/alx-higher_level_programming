21b94e6faa370a7421311a541170de1f5f6b03e0
Jaba <jr.gamachu@gmail.com>
2023-01-14 03:09:45 +0530

    Esrever

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -3,10 +3,10 @@
 // Returns the reversed version of a list
 
 exports.esrever = function (list) {
-	arr = [];
+  const arr = [];
 
-	for (let i = list.length - 1; i >= 0; i--) {
-		arr.push(list[i]);
-	}
-	return arr;
+  for (let i = list.length - 1; i >= 0; i--) {
+    arr.push(list[i]);
+  }
+  return arr;
 };
