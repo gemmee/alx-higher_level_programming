8021b79070b7b41015d396c680ee538dc3d30623
Jaba <jr.gamachu@gmail.com>
2023-01-14 00:02:47 +0530

    Rectangle #3

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,9 +1,18 @@
 #!/usr/bin/node
+
+// Prints a Rectangle of 'X' with the parameters passed as its dimension
+
 module.exports = class Rectangle {
   constructor (w, h) {
     if (typeof w === 'number' && typeof h === 'number' && w > 0 && h > 0) {
       this.width = w;
       this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; ++i) {
+      console.log('X'.repeat(this.width));
     }
   }
 };
