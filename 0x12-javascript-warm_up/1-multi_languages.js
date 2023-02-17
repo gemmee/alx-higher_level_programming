9b25e75b6a02894fed325acb56b2f42ed30777f3
Jeba <jr.gamachu@gmail.com>
2023-02-17 20:41:55 +0530

    Prints cool message

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,4 +1,2 @@
 #!/usr/bin/node
-// Script that prints a cool message
-const praise = 'C is fun\nPython is cool\nJavascript is amazing';
-console.log(praise);
+console.log('C is fun\nPython is cool\nJavascript is amazing');
