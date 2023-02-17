8a68e62b0e586408592e0ad207bd35837a969eee
Jeba <jr.gamachu@gmail.com>
2023-02-17 17:19:34 +0530

    Appends to a file

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+This module is 2-append_write.
+
+It has one function that appends a string at the end of a text file
+and returns the number of characters added.
+"""
+
+
+def append_write(filename="", text=""):
+    """Appends a string at the end of a text file.
+
+    Args:
+        filename - the text file to be modified
+                    It will be created if it doesn't exist.
+        text - the string to be appended to the file.
+    Returns:
+        the number of characters added.
+    """
+    with open('filename', mode="a+", encoding="utf-8") as f:
+        return f.write(text)
