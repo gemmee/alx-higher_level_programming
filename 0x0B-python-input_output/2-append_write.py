771a1565008ff90450845d8aa8f7da39a1931c58
Jeba <jr.gamachu@gmail.com>
2023-02-17 17:27:40 +0530

    Appends to a file

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -17,5 +17,5 @@ def append_write(filename="", text=""):
     Returns:
         the number of characters added.
     """
-    with open('filename', mode="a+", encoding="utf-8") as f:
+    with open(filename, "a+", encoding="utf-8") as f:
         return f.write(text)
