f31f40d627894c98560a22b34b2c0aa016d05d5d
gamachu <jr.gamachu@gmail.com>
2023-07-21 00:11:31 +0530

    initial commit

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,5 @@
+-- lists all privileges of the mysql user 'user_0d_1'
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+
+-- lists all privileges of the mysql user 'user_0d_2'
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
