81646d6cbbba8f6f5b20390ed0f7f97c6b7464b8
Gamachu <jr.gamachu@gmail.com>
2024-01-18 18:40:27 +0530

    change

--- 0x00-python-hello_world/5-print_string.py
+++ 0x00-python-hello_world/5-print_string.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
 str = "Holberton School"
-print("{}{}{}\n{}".format(str, str, str, str[0:9]))
+print("%s%s%s\n%s"%(str,str,str,str[:9]))
+#print(f"{str}{str}{str}\n{str[:9]}")
+#print("{}{}{}\n{}".format(str, str, str, str[0:9]))
