b441eaf08abed669de80db078acbec9db507763a
Gamachu <jr.gamachu@gmail.com>
2024-01-19 13:02:55 +0530

    Print alphabet except qe

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+for ascii in range(97, 123):
+    print("{}".format("" if chr(ascii) in "qe" else chr(ascii)), end='')
+'''
 for i in range(ord('a'), ord('z') + 1):
     if (i != ord('q') and i != ord('e')):
         print("{}".format(chr(i)), end="")
+'''
