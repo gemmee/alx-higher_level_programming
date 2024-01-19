3060c058ee2861f461ab2a51a194e65cb8dc8742
Gamachu <jr.gamachu@gmail.com>
2024-01-19 12:47:36 +0530

    Print the ASCII alphabet

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
+# for i in range(97, 123):
 for i in range(ord('a'), ord('z') + 1):
-    print('{}'.format(chr(i)), end="")
+    # print('{}'.format(chr(i)), end="")
+    print(f'{chr(i)}, end="")
