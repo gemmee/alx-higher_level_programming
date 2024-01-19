729a36acd041aeb344397052d7b623c8b8eb30b2
Gamachu <jr.gamachu@gmail.com>
2024-01-19 14:01:34 +0530

    Print 00 to 99 with one print

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 for i in range(0, 100):
-    sep = "\n" if i == 99 else ", "
-    print(f"{i:02}{sep}", end="")
+    print(f"{i:02}" + "{}".format("\n" if i == 99 else ", "), end="")
