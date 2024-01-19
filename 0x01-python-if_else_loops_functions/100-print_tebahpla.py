c35d1b1161ca37f63abfd7ed3fb3769427facbd4
Gamachu <jr.gamachu@gmail.com>
2024-01-19 18:32:04 +0530

    Print the alternating alphabet in reverse

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,13 +1,3 @@
 #!/usr/bin/python3
-
-def printReverse():
-    '''
-    prints the ASCII alphabet, in reverse order, alternating lowercase
-    and uppercase.
-
-    '''
-    for i in range(90, 64, -1):
-        print("{}".format(chr(i) if i % 2 else chr(i + 32)), end='')
-
-
-printReverse()
+for i in range(ord('z'), ord('a') - 1, -1):
+    print("{}".format(chr(i - 32) if i % 2 else chr(i), end=''))
