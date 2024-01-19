ba8b614c95061b79ef7d65457631dfce343eb4af
Gamachu <jr.gamachu@gmail.com>
2024-01-19 18:34:26 +0530

    Print the alternating alphabet in reverse

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 for i in range(ord('z'), ord('a') - 1, -1):
-    print("{}".format(chr(i - 32) if i % 2 else chr(i), end=''))
+    print("{}".format(chr(i - 32) if i % 2 else chr(i)), end='')
