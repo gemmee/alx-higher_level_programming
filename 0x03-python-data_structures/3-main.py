d168c385f4f226528ccbe7a26d9a8a1184f13298
Gamachu <jr.gamachu@gmail.com>
2024-01-21 00:50:13 +0530

    Print list in reverse order

--- 0x03-python-data_structures/3-main.py
+++ 0x03-python-data_structures/3-main.py
@@ -3,3 +3,12 @@ print_reversed_list_integer = __import__('3-print_reversed_list_integer').print_
 
 my_list = [1, 2, 3, 4, 5]
 print_reversed_list_integer(my_list)
+
+list = None
+print_reversed_list_integer(list)
+
+list2 = [1, 2, "H", 9]
+print_reversed_list_integer(list2)
+
+list3 = []
+print_reversed_list_integer(list3)
