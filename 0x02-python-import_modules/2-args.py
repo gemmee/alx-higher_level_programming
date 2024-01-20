2158137f00780a2b2b646584d06aeb2f5b7f2914
Gamachu <jr.gamachu@gmail.com>
2024-01-20 13:26:56 +0530

    Make a dynamic script using argv

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,16 +1,12 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    import sys
-    argv = sys.argv[1:]
-    argv_count = len(argv)
-    index = 1
-    if argv_count == 0:
-        print("{:d} arguments.".format(argv_count))
-    elif argv_count == 1:
-        print("{:d} argument:".format(argv_count))
-        print("{:d}: {:s}".format(index, sys.argv[1]))
+if __name__ == '__main__':
+    from sys import argv
+    no_args = len(argv) - 1
+    if not no_args:
+        end = 's.'
     else:
-        print("{:d} arguments:".format(argv_count))
-        while index <= argv_count:
-            print("{:d}: {:s}".format(index, sys.argv[index]))
-            index += 1
+        end = "{}:".format("s" if no_args > 1 else "")
+    print('{:d} argument{}'.format(no_args, end))
+    if no_args:
+        for i in range(no_args):
+            print('{:d}: {}'.format(i+1, argv[i+1]))
