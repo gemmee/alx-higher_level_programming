4f4651020ad35763587f1a1de1cc73fcceec94b5
Gamachu <jr.gamachu@gmail.com>
2024-01-22 15:50:47 +0530

    8-simple_delete.py

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,15 +1,5 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
-    """
-    Deletes a key in a dictionary.
-
-    key: the key which is always a string
-    a_dictionary: the dictionary
-
-    Description:
-        If a key doesn’t exist, the dictionary won’t change
-        You are not allowed to import any module
-    """
-    if key in a_dictionary.keys():
+    if key in a_dictionary:
         del a_dictionary[key]
     return a_dictionary
