bee3f36e47d25344cc9a3171981ebc96045b6b88
Gamachu <jr.gamachu@gmail.com>
2024-01-22 14:03:13 +0530

    Search and replace in a list

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,15 +1,6 @@
 #!/usr/bin/python3
-
 def search_replace(my_list, search, replace):
-    '''
-    Replaces all occurrences of an element by another in a new list.
-
-    my_list: the list
-    search: the element to be replaced
-    replace: the new element
-    '''
-    new_list = list(my_list)
-    for i in range(len(new_list)):
-        if search == new_list[i]:
-            new_list[i] = replace
+    new_list = my_list[:]
+    while search in new_list:
+        new_list[new_list.index(search)] = replace
     return new_list
