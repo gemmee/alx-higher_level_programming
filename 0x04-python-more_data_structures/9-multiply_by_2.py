ff803784bc228d23590b91c86f4360f00e974181
Gamachu <jr.gamachu@gmail.com>
2024-01-22 15:57:11 +0530

    Return new dictionary with double values

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,12 +1,6 @@
 #!/usr/bin/python3
-
 def multiply_by_2(a_dictionary):
-    """
-    Returns a new dictionary with all values multiplied by 2.
-
-    a_dictionary: the dictionary with integers values
-    """
     new_dict = {}
-    for k in a_dictionary:
-        new_dict[k] = a_dictionary[k] * 2
+    for k, v in a_dictionary.items():
+        new_dict[k] = v * 2
     return new_dict
