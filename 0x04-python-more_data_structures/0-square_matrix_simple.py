d94e0484d904a16e531864d317b344c5b061a469
Gamachu <jr.gamachu@gmail.com>
2024-01-22 13:43:42 +0530

    Compute the square of elements in matrix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,13 +1,3 @@
 #!/usr/bin/python3
-
 def square_matrix_simple(matrix=[]):
-    '''
-    compute the square value of alll integers of a matrix
-    '''
-    sqr = []
-    for i in range(len(matrix)):
-        row = []
-        for j in range(len(matrix[i])):
-            row.append(matrix[i][j] ** 2)
-        sqr.append(row)
-    return sqr
+    return [[i**2 for i in matrix[i]] for i in range(len(matrix))]
