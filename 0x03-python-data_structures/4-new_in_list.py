a684fa5c0c1d6db78581ac5c78c8a7bf5b9b62e5
Gamachu <jr.gamachu@gmail.com>
2024-03-21 19:45:48 +0530

    Replace in a copy

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,13 +1,5 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    """
-    Replaces an element in a list at a specific position without modifying
-    the original list(like in C).
-
-    my_list: reference to the original list
-    idx: index of at which element is replaced
-    element: new element for replace
-    """
     copy_list = my_list[:]  # same as my_list.copy()
     if 0 <= idx < len(my_list):
         copy_list[idx] = element
