9aa2c80a07b981229068021b415956e0c520325d
Gamachu <jr.gamachu@gmail.com>
2024-04-04 14:18:17 +0530

    Coordinates of a square

---
+++ 0x06-python-classes/6-main.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+Square = __import__('6-square').Square
+
+my_square_1 = Square(3)
+my_square_1.my_print()
+
+print("--")
+
+my_square_2 = Square(3, (1, 1))
+my_square_2.my_print()
+
+print("--")
+
+my_square_3 = Square(3, (3, 0))
+my_square_3.my_print()
+
+print("--")
+
+try:
+    my_square_3 = Square(3, (1, -3))
+    my_square_3.my_print()
+except Exception as e:
+    print(e)
+
+try:
+	my_sq = Square(4, (1, 3))
+	my_sq.my_print()
+except Exception as e:
+	print(e)
