cd33e98e81f5b82a406439252b62143ab345477a
Gamachu <jr.gamachu@gmail.com>
2024-04-14 06:15:07 +0530

    String representation

---
+++ 0x08-python-more_classes/3-rectangle.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python3
+'''
+This is '3-rectangle' module.
+
+This module contains 1 class Rectangle
+'''
+
+
+class Rectangle:
+    '''
+    Defines a Rectangle class
+
+    **Instance attributes**
+    width: private, must be a non-negative int
+    height: private, must be a non-negative int
+
+    **Public instance methods**
+    area(self)
+    perimeter(self)
+    '''
+
+    def __init__(self, width=0, height=0):
+        '''
+        Instantiates a Rectangle
+
+        Args:
+            width: optional, a non-negative int
+            height: optional, a non-negative int
+        '''
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @property
+    def height(self):
+        return self.__height
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        return self.__width * self.__height
+
+    def perimeter(self):
+        if (self.__width == 0) or (self.__height == 0):
+            return 0
+        return 2 * (self.__width + self.__height)
+
+    def __str__(self):
+        if (self.__width == 0) or (self.__height == 0):
+            return ""
+        return '\n'.join(['#'*self.__width for h in range(self.__height)])
