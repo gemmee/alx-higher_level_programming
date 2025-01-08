bd66e060a44b88010d59c926af9c6f43680e6c43
Gamachu Abara <jr.gamachu@gmail.com>
2025-01-08 16:32:11 +0300

    Insert a record into a table

---
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -0,0 +1,5 @@
+-- inserts a new row in the table `first_table` in a db in the MySQL server.
+-- the db name is passed as an argument of the mysql command
+-- New row info: id = 89 and name = Best School
+
+INSERT INTO first_table(id, name) VALUES(89, "Best School");
