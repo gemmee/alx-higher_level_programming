1afcb4831456b79d519c568ac5c053e96a941d9d
Gamachu Abara <jr.gamachu@gmail.com>
2025-01-08 16:22:39 +0300

    List all rows in table

---
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -0,0 +1,5 @@
+-- lists all rows of the table `first_table` from a db in the MySQL server.
+-- the db name is passed as an argument to the mysql command
+-- all fields should be printed
+
+SELECT * FROM first_table;
