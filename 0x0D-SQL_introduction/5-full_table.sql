d557b1f44b98dbfae090486c5c871822b4f373a0
Gamachu Abara <jr.gamachu@gmail.com>
2025-01-08 16:15:18 +0300

    Get full description of table

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -2,5 +2,6 @@
 -- the db name is passed as an argument of the mysql command
 -- usage of `DESCRIBE` or `EXPLAIN` statements is not allowed
 -- DESCRIBE first_table;
--- SHOW CREATE TABLE first_table;
-SHOW COLUMNS FROM first_table;
+-- SHOW COLUMNS FROM first_table;
+
+SHOW CREATE TABLE first_table;
