f23609434a3e1a87d89f26c919a35b86c90693f3
Gamachu Abara <jr.gamachu@gmail.com>
2025-01-08 16:12:36 +0300

    Get full description

---
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -0,0 +1,6 @@
+-- prints the full description of the table `first_table` from a db in the MySQL server.
+-- the db name is passed as an argument of the mysql command
+-- usage of `DESCRIBE` or `EXPLAIN` statements is not allowed
+-- DESCRIBE first_table;
+-- SHOW CREATE TABLE first_table;
+SHOW COLUMNS FROM first_table;
