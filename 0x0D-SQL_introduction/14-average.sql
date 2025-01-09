23f099d7192fb34c47b3ce88ea77c34c12835387
Gamachu Abara <jr.gamachu@gmail.com>
2025-01-09 06:31:25 +0300

    Compute average of scores

---
+++ 0x0D-SQL_introduction/14-average.sql
@@ -0,0 +1,5 @@
+-- computes the score average of all records in the table `second_table`
+-- the result column name should be average
+-- the db name is passed as an argument of the mysql command
+
+SELECT AVG(score) AS average FROM second_table;
