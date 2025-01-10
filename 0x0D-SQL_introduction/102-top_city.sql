2d565b97598caf9da231ae51251c93e1cc39addb
Gamachu Abara <jr.gamachu@gmail.com>
2025-01-11 00:57:08 +0300

    Display the top 3 cities in temp during Jul and Aug

---
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -0,0 +1,13 @@
+-- imports in hbtn_0c_0 db `temperatures.sql` table dump which
+-- is downloaded in the same folder as this script
+-- displays the top 3 of cities temp during Jul.(7) and Aug.(8)
+-- ordered by temp (desc)
+
+USE hbtn_0c_0;
+SOURCE ./temperatures.sql;
+SELECT city, AVG(value) AS avg_temp
+	FROM temperatures
+	WHERE month = 7 OR month = 8
+	GROUP BY city
+	ORDER BY avg_temp DESC
+	LIMIT 3;
