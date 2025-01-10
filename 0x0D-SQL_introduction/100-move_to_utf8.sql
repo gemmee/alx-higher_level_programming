d58e3af9b415c3110e09ef8a00303268b7dd81f4
Gamachu Abara <jr.gamachu@gmail.com>
2025-01-10 11:42:53 +0300

    Change to UTF8mb4

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -7,11 +7,15 @@
 -- Place: occ, Kaliti
 
 -- Change the database character set and collation to utf8mb4
-ALTER DATABASE `hbtn_0c_0` CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER DATABASE `hbtn_0c_0`
+	CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- Change the table character set and collation to utf8mb4
-ALTER TABLE `first_table` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE `first_table`
+	CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- Modify the 'name' column to use utf8mb4 explicitly
+-- The ff is not needed as changing the table do the same
 ALTER TABLE `first_table`
-	CHANGE COLUMN `name` `name` VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+	CHANGE COLUMN `name` `name` VARCHAR(256)
+	CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
