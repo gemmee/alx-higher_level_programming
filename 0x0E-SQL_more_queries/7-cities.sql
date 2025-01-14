ec6e1877f38f8c8837fdd2490a24732326c5babd
Gamachu Abara <jr.gamachu@gmail.com>
2025-01-14 10:06:37 +0300

    Exchange the position of fields

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -13,7 +13,7 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS cities (
 	id INT AUTO_INCREMENT PRIMARY KEY, -- implicitly NOT NULL and UNIQUE
-	name VARCHAR(256) NOT NULL,
 	state_id INT NOT NULL,
+	name VARCHAR(256) NOT NULL,
 	FOREIGN KEY (state_id) REFERENCES states(id)
 );
