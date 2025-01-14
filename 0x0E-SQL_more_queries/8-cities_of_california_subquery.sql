e3f0235b7c511aabdabf697e0a1ef186933e0980
Gamachu Abara <jr.gamachu@gmail.com>
2025-01-14 11:36:41 +0300

    Use subquery to list cities of CAL

---
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,15 @@
+-- lists all the cities of California that are in db `hbtn_0d_usa`
+-- the states table contains only one record where name = California
+-- results must be sorted in ascending order by cities.id
+-- usage of the JOIN keyword is not allowed
+-- the db name is passed as an arg of the mysql command.
+
+-- +---------------------+
+-- | Author: Gamachu     |
+-- | Place: occ, Kaliti  |
+-- +---------------------+
+
+-- query using subquery
+SELECT id, name
+	FROM cities
+	WHERE state_id = (SELECT id FROM states WHERE name = 'California');
