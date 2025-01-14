fddbf036e2d036dfe43c11edb6e7523af0994141
Gamachu Abara <jr.gamachu@gmail.com>
2025-01-14 13:07:11 +0300

    List all cities with their states

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -9,21 +9,21 @@
 -- | Place: occ, Kaliti  |
 -- +---------------------+
 
--- query using INNER JOIN
-SELECT cities.id, cities.name, states.name
-	FROM cities INNER JOIN states
-	ON states.id = cities.state_id
-	ORDER BY cities.id;
 
 -- equivalent query using comma-separated syntax (less clear)
---SELECT cities.id, cities.name, states.name
+-- SELECT cities.id, cities.name, states.name
 --	FROM cities, states  -- implicit cartesian join
 --	WHERE states.id = cities.state_id
 --	ORDER BY cities.id;
 
 -- equivalent query using CROSS JOIN (less efficient)
---SELECT cities.id, cities.name, states.name
+-- SELECT cities.id, cities.name, states.name
 --	FROM cities CROSS JOIN states
 --	WHERE states.id = cities.state_id
 --	ORDER BY cities.id;
 
+-- query using INNER JOIN
+SELECT cities.id, cities.name, states.name
+	FROM cities INNER JOIN states
+	ON states.id = cities.state_id
+	ORDER BY cities.id;
