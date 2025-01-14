c3f7846a251c6653d7c2d359a1f41122bccc66be
Gamachu Abara <jr.gamachu@gmail.com>
2025-01-14 15:15:34 +0300

    Remove one select statement

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -9,8 +9,20 @@
 -- | Place: occ, Kaliti  |
 -- +---------------------+
 
-SELECT name
-	FROM tv_genres INNER JOIN tv_show_genres
+
+SELECT tv_genres.name
+	FROM tv_genres
+	INNER JOIN tv_show_genres
 	ON tv_genres.id = tv_show_genres.genre_id
-	WHERE tv_show_genres.show_id = (SELECT id FROM tv_shows WHERE title = 'Dexter')
-	ORDER BY name;
+	INNER JOIN tv_shows
+	ON tv_shows.id = tv_show_genres.show_id
+	WHERE tv_shows.title = 'Dexter'
+	ORDER BY tv_genres.name;
+
+-- the ff query using subquery also works but
+-- it breaks the requirement by using SELECT two times
+-- SELECT name
+--	FROM tv_genres INNER JOIN tv_show_genres
+--	ON tv_genres.id = tv_show_genres.genre_id
+--	WHERE tv_show_genres.show_id = (SELECT id FROM tv_shows WHERE title = 'Dexter')
+--	ORDER BY name;
