4ed1bc9df367e1e5614c4baa837e0c6853aec938
Gamachu Abara <jr.gamachu@gmail.com>
2025-03-01 23:33:00 +0300

    Print 3 cool messages

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+// a script that prints 3 lines with
+// first line saying 'C is fun'
+// second line saying 'Python is cool'
+// third line saying 'JavaScript is amazing'
+
+
+console.log('C is fun')
+console.log('Python is cool')
+console.log('JavaScript is amazing')
