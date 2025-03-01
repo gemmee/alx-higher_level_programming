82079bdb3c0f5e1ce7cfc145feff5eb856797375
Gamachu Abara <jr.gamachu@gmail.com>
2025-03-01 23:34:40 +0300

    Print 3 cool things

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -6,6 +6,6 @@
 // third line saying 'JavaScript is amazing'
 
 
-console.log('C is fun')
-console.log('Python is cool')
-console.log('JavaScript is amazing')
+console.log('C is fun');
+console.log('Python is cool');
+console.log('JavaScript is amazing');
