96676aee7158cbe3942ffafffb5b4ade6f97af70
Gamachu <jr.gamachu@gmail.com>
2025-03-19 00:45:42 +0300

    Print the first argument

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,10 +1,12 @@
 #!/usr/bin/node
+
 // prints the first argument passed to it
 // if no arguments -> prints "No argument"
 // usage of var and length are not allowed
 
-/* surprise ... Boolean([]) is true,
- * so I used Boolean([][0]) which is false in this case * and Boolean(undefined) is false */
+// surprise ... Boolean([]) is true,
+// so I used Boolean([][0]) which is false in this case
+// and Boolean(undefined) is false
 
 const args = process.argv.slice(2);
 const firstArg = args[0] ? args[0] : 'No argument';
