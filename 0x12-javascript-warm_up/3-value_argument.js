3837bba714e3e081b1d7718fca868472fca12210
Gamachu <jr.gamachu@gmail.com>
2025-03-19 00:33:55 +0300

    Print the first argument

---
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -0,0 +1,9 @@
+#!/bin/env node
+// prints the first argument passed to it
+// if no arguments -> prints "No argument"
+// usage of var and length are not allowed
+
+/* surprise ... Boolean([]) is true,
+ * so I used Boolean([][0]) which is false in this case * and Boolean(undefined) is false */
+
+process.argv.slice(2)[0] ? console.log(process.argv[2]) : console.log('No argument');
