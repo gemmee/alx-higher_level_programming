c2144519a11aa1f8af6f3ae043cf9634379b2a45
Gamachu Abara <jr.gamachu@gmail.com>
2025-04-27 16:23:43 +0300

    Let see this one

--- 0x12-javascript-warm_up/6-multi_language_loop.js
+++ 0x12-javascript-warm_up/6-multi_language_loop.js
@@ -1,8 +1,10 @@
 #!/usr/bin/node
 
+// script to print strings using a loop
+
 let i;
-const messages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+const myString = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
 for (i = 0; i < 3; i++) {
-  console.log(messages[i]);
+  console.log(myString[i]);
 }
